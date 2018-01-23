e469b5186990eb49ed79f917cb3e44bf645aa9ea
uncleoptimus <uncle.optimus@gmail.com>
2018-01-22 22:19:57 -0800

    Add basic form fields and submit btn to Settings sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,5 +1,12 @@
 import React from 'react';
 // import Grid from 'material-ui-next/Grid';
+
+import Typography from 'material-ui-next/Typography';
+
+import Button from 'material-ui-next/Button';
+import Input, { InputLabel, InputAdornment } from 'material-ui-next/Input';
+import { FormControl, FormHelperText } from 'material-ui-next/Form';
+
 import { withStyles } from 'material-ui-next/styles';
 
 import Drawer from 'material-ui-next/Drawer';
@@ -16,7 +23,8 @@ const styles = theme => ({
     // position: 'relative',
     position: 'absolute',
     height: '100%',
-    width: 320
+    width: 320,
+    padding: 20
   },
   drawerHeader: {
     display: 'flex',
@@ -24,6 +32,19 @@ const styles = theme => ({
     justifyContent: 'flex-end',
     padding: '0 8px',
     ...theme.mixins.toolbar
+  },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  formControl: {
+    margin: theme.spacing.unit
+  },
+  withoutLabel: {
+    marginTop: theme.spacing.unit * 3
+  },
+  button: {
+    margin: theme.spacing.unit
   }
 });
 
@@ -38,14 +59,66 @@ const Sidebar = ({ classes, open, handleDrawerClose }) => (
   >
     <div className={classes.drawerInner}>
       <div className={classes.drawerHeader}>
+        <Typography type="title" color="inherit">
+          Configuration
+        </Typography>
         <IconButton onClick={handleDrawerClose}>
           <DirectionsRun />
         </IconButton>
       </div>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="settings-num-sets">Tabatas</InputLabel>
+        <Input
+          id="settings-num-sets"
+          value={1}
+          onChange={() => console.log('changed!')}
+          endAdornment={<InputAdornment position="start">sets</InputAdornment>}
+        />
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="settings-num-intervals">Tabatas</InputLabel>
+        <Input
+          id="settings-num-intervals"
+          value={8}
+          onChange={() => console.log('changed!')}
+          endAdornment={
+            <InputAdornment position="start">intervals</InputAdornment>
+          }
+        />
+      </FormControl>
+      <FormControl
+        className={`${classes.formControl} ${classes.withoutLabel}`}
+        aria-describedby="interval-length-seconds"
+      >
+        <FormHelperText id="intervals-helper-text">
+          Interval length
+        </FormHelperText>
+        <Input
+          id="settings-time-interval"
+          value={20}
+          onChange={() => console.log('changed!')}
+          endAdornment={<InputAdornment position="end">Sec</InputAdornment>}
+        />
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="settings-time-rest">Interval rest time</InputLabel>
+        <Input
+          id="settings-time-rest"
+          value={10}
+          onChange={() => console.log('changed!')}
+          endAdornment={<InputAdornment position="start">Sec</InputAdornment>}
+        />
+      </FormControl>
       <Divider />
-      <h4>One</h4>
-      <h4>Two</h4>
-      <h4>Three</h4>
+      <Button className={classes.button} raised color="secondary">
+        RESET
+      </Button>
+      <Button className={classes.button} raised color="primary">
+        CONFIRM
+      </Button>
+      <Typography type="body2" gutterBottom>
+        On CONFIRM current timer will be reset to start
+      </Typography>
     </div>
   </Drawer>
 );
